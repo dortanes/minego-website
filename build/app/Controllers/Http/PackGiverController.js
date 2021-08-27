27283ab60619d79b12a973b17161dc7f61d85cde
sarangdev <1devout0@gmail.com>
2021-08-28 04:59:53 +1000

    Update

--- build/app/Controllers/Http/PackGiverController.js
+++ build/app/Controllers/Http/PackGiverController.js
@@ -1,9 +1,28 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const rcon_client_1 = require("rcon-client");
+const Rcon = __importStar(require("rcon"));
 const Pack_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Models/Pack"));
 class PackGiverController {
     async execute({ nickname, packId }) {
@@ -13,8 +32,22 @@ class PackGiverController {
             .replace('%nick', nickname)
             .replace('%group', String(pack?.group));
         console.trace(credentials, cmd);
-        const rcon = await rcon_client_1.RconClient.connect(credentials[1].split(':')[0], Number(credentials[1].split(':')[1]), credentials[0]);
-        return await rcon.send(cmd);
+        return await new Promise((resolve, reject) => {
+            const rcon = new Rcon(credentials[1].split(':')[0], Number(credentials[1].split(':')[1]), credentials[0]);
+            rcon.on('auth', () => {
+                console.log("rcon >> authenticated! sending command", cmd);
+                rcon.send(cmd);
+            }).on('response', (str) => {
+                console.log('rcon >> response:', str);
+                resolve(true);
+            }).on('error', (err) => {
+                console.error(err);
+                reject(err);
+            }).on('end', () => {
+                console.log('rcon >> connection closed');
+            });
+            rcon.connect();
+        });
     }
 }
 exports.default = PackGiverController;
